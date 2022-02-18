cee4960814c76983447168a2a7d4ea892f3b9f87
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-18 14:08:31 +0100

    api jeu-video

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,7 @@ nunjucks.configure("views", { autoescape: true, express: app });
 
 app.set("view engine", "njk");
 
-//Route Home.njk
-app.get("/home", (request, response) => {
-  apivideo("http://videogame-api.fly.dev/games/", (error, body: string) => {
-    //apivideo("http://videogame-api.fly.dev/platforms/", (error, body: string) => {
-    if (error) {
-      throw error;
-    }
-    const apiResponse = JSON.parse(body);
-    const games = apiResponse.games;
-    response.render("home", { gamesName: games });
-  });
-});
-
-//Route platform.njk
+//Route platform.njk au clic renvoi a la page home
 app.get("/", (request, response) => {
   apivideo("http://videogame-api.fly.dev/platforms/", (error, body: string) => {
     if (error) {
@@ -32,34 +19,75 @@ app.get("/", (request, response) => {
   });
 });
 
-// Route test.njk
-app.get("/test", (request, response) => {
-  apivideo("http://videogame-api.fly.dev/genres", (error, body: string) => {
+//Route Home.njk aaffiche liste jeu
+app.get("/home/:id_platform", (request, response) => {
+  const idParam = request.params.id_platform;
+  //console.log(idParam);
+  apivideo(`http://videogame-api.fly.dev/games/platforms/${idParam}`, (error, body: string) => {
+    //apivideo("http://videogame-api.fly.dev/platforms/", (error, body: string) => {
     if (error) {
       throw error;
     }
-    const apiTest = JSON.parse(body);
-    const platfo = apiTest.genres;
-    console.log(apiTest.platforms);
-    response.render("test", { testName: platfo });
+    //console.log(body);
+
+    const apiResponse = JSON.parse(body);
+    //console.log(apiResponse);
+
+    const games = apiResponse.games;
+    response.render("home", { gamesName: games, paraId: idParam });
   });
 });
+
+app.get("/test/:id_platform", (request, response) => {
+  const idParams = request.params.id_platform;
+  console.log(idParams);
+  apivideo(`http://videogame-api.fly.dev/games/platforms/${idParams}`, (error, body: string) => {
+    //apivideo("http://videogame-api.fly.dev/platforms/", (error, body: string) => {
+    if (error) {
+      throw error;
+    }
+    //console.log(body);
+
+    const apiResponses = JSON.parse(body);
+    //console.log(apiResponse);
+
+    const game = apiResponses.games;
+    response.render("test", { gamesNames: game, paraId: idParams });
+  });
+});
+
+// Route test.njk
+// app.get("/test/:slug_games", (request, response) => {
+//   const idGames = request.params.slug_games;
+//   console.log(idGames);
+
+//   apivideo(`http://videogame-api.fly.dev/games/platforms/${idGames}`, (error, body: string) => {
+//     if (error) {
+//       throw error;
+//     }
+//     const apiTest = JSON.parse(body);
+//     console.log(apiTest);
+
+//     const platfo = apiTest.genres;
+//     console.log(platfo);
+
+//     //console.log(apiTest.platforms);
+//     response.render("test", { testName: platfo, paraSlug: idGames });
+//   });
+// });
 
 //Route onePlatform.njk
-app.get("/onePlatform", (request, response) => {
-  apivideo("http://videogame-api.fly.dev/platforms/", (error, body: string) => {
-    if (error) {
-      throw error;
-    }
-    const apiEssais = JSON.parse(body);
-    const one = apiEssais.platforms;
-    //console.log(apiEssais);
+// app.get("/onePlatform", (request, response) => {
+//   apivideo("http://videogame-api.fly.dev/platform/", (error, body: string) => {
+//     if (error) {
+//       throw error;
+//     }
+//     const apiEssais = JSON.parse(body);
+//     const one = apiEssais.platforms;
 
-    console.log(one);
-
-    response.render("onePlatform", { oneName: one });
-  });
-});
+//     response.render("onePlatform", { oneName: one });
+//   });
+// });
 
 // locahost 3000
 app.listen(3000, () => {
